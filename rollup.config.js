64e8b1153bbff29de1d697a93f683ce654ca2a63
Seitmagambet Olzhas <sei7magambet@gmail.com>
2019-06-17 21:58:24 +0600

    changed locale api

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,35 @@
 import { terser } from 'rollup-plugin-terser';
 import buble from 'rollup-plugin-buble';
+import fs from 'fs';
+import path from 'path';
 
 const production = process.env.NODE_ENV === 'production';
 
-export default {
-  input: 'lib/index.js',
+const localesList = fs.readdirSync(path.join(__dirname, 'lib', 'locale'));
+const locales = localesList.map(createLocaleConfig);
+
+function createLocaleConfig(locale) {
+  return {
+    input: path.join('lib', 'locale', locale),
+    output: {
+      format: 'es',
+      file: path.join('dist', 'locale', locale)
+    },
+    plugins: [
+      buble(),
+      production && terser()
+    ]
+  };
+}
+
+export default [{
+  input: path.join('lib', 'index.js'),
   output: {
     format: 'es',
-    file: 'dist/index.js'
+    file: path.join('dist', 'index.js')
   },
   plugins: [
     buble(),
     production && terser()
   ]
-};
+}, ...locales];
