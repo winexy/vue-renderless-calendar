24cf1c59afecb8fe70abe3432f27f29739b8445e
maxim <tyminko@gmail.com>
2020-02-26 22:31:32 +0100

    update RenderlessCalender.js (use getCalendarStringsForLocale) + update examples App.vue

--- example/App.vue
+++ example/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="app">
     <section>
-      <Calendar />
+      <Calendar locale="fr" />
     </section>
     <section>
-      <DoubleCalendar />
+      <DoubleCalendar locale="de" />
     </section>
     <section>
-      <InfiniteCalendar />
+      <InfiniteCalendar locale="ru" />
     </section>
   </div>
 </template>
