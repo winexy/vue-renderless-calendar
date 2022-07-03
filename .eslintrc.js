a4ae6e37d20235dc9cd20b9f774c306ac27e0a6d
Olzhas Seitmagambet <sei7magambet@gmail.com>
2022-07-03 15:37:23 +0600

    chore: extract eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ['chocotravel-aviata', 'prettier'],
+  rules: {},
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+};
