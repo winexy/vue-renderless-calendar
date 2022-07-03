1fdf8e0ded6058eb2d88f11254b8257928354ec7
Olzhas Seitmagambet <sei7magambet@gmail.com>
2022-07-03 15:33:02 +0600

    refactor: decompose vue2 example

--- examples/vue2/main.js
+++ examples/vue2/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
-import { RenderlessCalendar } from '../lib';
+import { RenderlessCalendar } from '../../lib';
 
 Vue.config.productionTip = false;
 
