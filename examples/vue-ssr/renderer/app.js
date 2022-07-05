3842c63239653d268dc09fd4a355d534e7e3b042
Olzhas Seitmagambet <sei7magambet@gmail.com>
2022-07-05 23:00:35 +0600

    chore: add vue-ssr boilerplate

---
+++ examples/vue-ssr/renderer/app.js
@@ -0,0 +1,20 @@
+import { createSSRApp, h } from 'vue';
+import { setPageContext } from './usePageContext';
+
+export { createApp };
+
+function createApp(pageContext) {
+  const { Page, pageProps } = pageContext;
+  const PageWithLayout = {
+    render() {
+      return h(Page, pageProps || {});
+    },
+  };
+
+  const app = createSSRApp(PageWithLayout);
+
+  // We make `pageContext` available from any Vue component
+  setPageContext(app, pageContext);
+
+  return app;
+}
